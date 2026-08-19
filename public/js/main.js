8e66cc20da98e34aa956d5e302c0e3feec1f63e2
Hasagiiiii <umaduplagamer@gmail.com>
2026-08-19 11:39:51 -0300

    Liga controle isolado de destaque de notícias

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
 import { bindEvents } from './events.js';
 import { installEnhancements } from './enhancements.js';
 import { installLibraryPolish } from './library-polish.js';
+import { installAdminNewsFeature } from './admin-news-feature.js';
 import { render } from './router.js';
 
 bindEvents();
 installEnhancements();
 installLibraryPolish();
+installAdminNewsFeature();
 render();
