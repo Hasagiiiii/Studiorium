6f29dbe17210831cc6d0f5b334c2ccdd1a59b188
Hasagiiiii <umaduplagamer@gmail.com>
2026-08-19 12:31:27 -0300

    Ativa gestão ADM de conteúdos publicados

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,12 @@ import { bindEvents } from './events.js';
 import { installEnhancements } from './enhancements.js';
 import { installLibraryPolish } from './library-polish.js';
 import { installAdminNewsFeature } from './admin-news-feature.js';
+import { installAdminPublishedActions } from './admin-published-actions-v329.js';
 import { render } from './router.js';
 
 bindEvents();
 installEnhancements();
 installLibraryPolish();
 installAdminNewsFeature();
+installAdminPublishedActions();
 render();
