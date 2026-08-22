95d6d8b8b8ce38c06900be3885d5dd2b1efe2548
Hasagiiiii <umaduplagamer@gmail.com>
2026-08-22 11:48:52 -0300

    feat(profile): route media through focused profile endpoint refs #63

---
+++ src/server/profile-media-router.js
@@ -0,0 +1,34 @@
+const { assertSameOrigin, send } = require('./http');
+const { apiPath } = require('./route-utils');
+const profileRoutes = require('./routes/profile');
+
+async function handleProfileMedia(req, res) {
+  const method = String(req.method || 'GET').toUpperCase();
+  const pathname = apiPath(req);
+
+  if (method === 'POST' && pathname === '/profile/media') {
+    assertSameOrigin(req);
+    return send(res, 200, await profileRoutes.uploadProfileMedia(req));
+  }
+
+  const ownMedia = pathname.match(/^\/profile\/media\/(avatar|cover)$/);
+  if (ownMedia && method === 'DELETE') {
+    assertSameOrigin(req);
+    return send(res, 200, await profileRoutes.removeProfileMedia(req, ownMedia[1]));
+  }
+
+  const publicMedia = pathname.match(/^\/profiles\/([^/]+)\/media\/(avatar|cover)$/);
+  if (publicMedia && method === 'GET') {
+    await profileRoutes.serveProfileMedia(
+      req,
+      res,
+      decodeURIComponent(publicMedia[1]),
+      publicMedia[2],
+    );
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = { handleProfileMedia };
